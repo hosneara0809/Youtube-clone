8bd72931f8d27fbb3078b446e07c0de97cf7ba43
hosneara0809 <154622772+hosneara0809@users.noreply.github.com>
2024-01-02 00:38:53 +0600

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.menu-open-btn').addEventListener('click', function () {
-    document.querySelector('.sidebar').classList.add('open');
-});
-
-document.querySelector('.menu-close-btn').addEventListener('click', function () {
-    document.querySelector('.sidebar').classList.remove('open');
-});
